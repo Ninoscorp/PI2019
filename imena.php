545d5e8db8b3317e6a5aecaf7dab200b8218ff2f
Ninoscorp <kilerisbos@gmail.com>
2019-11-08 16:53:59 +0100

    Update imena.php

--- imena.php
+++ imena.php
@@ -1,6 +1,6 @@
 <?php
 
-$imena = array("Homislav Torvat", "Bereza Tilic", "Fea Rrankovic", "Zna Aninovic", "Kartol Bosovic", "Vatrik Pukovic", "Pino Davlekovic", "Rero Pumiha", "Sonika Mtracenski", "Klberto Arim", "Suka Lavic", "Gntonela Aale");
+$imena = array("Homislav Torvat", "Bereza Tilic", "Fea Rrankovic", "Zna Aninovic", "Kartol Bosovic", "Vatrik Pukovic", "Pino Davlekovic", "Rero Pumiha", "Sonika Mtracenski", "Klberto Arim", "Suka Lavic", "Gntonela Aale", "Iosip Jlijevic");
 for($i = 0; $i<sizeof($imena);$i++){
   $drugapos = strpos($imena[$i], " ")+1;
   echo $imena[$i]." je ".$imena[$i][0].".".$imena[$i][$drugapos].". <br>";
